dd062ac6dc76821da9ec83b58aa2448ee78aa5b5
Sun Daowen <sundaowen@gmail.com>
2016-11-20 17:20:15 +0800

    windows

---
+++ zlib/make_windows_x86_64_msvc14.bat
@@ -0,0 +1,34 @@
+IF DEFINED VS140COMNTOOLS (
+  SET GENERATOR="Visual Studio 14 Win64"
+  SET target_lib_suffix=_msvc14
+)
+IF NOT DEFINED GENERATOR (
+  ECHO Can not find VC2015 installed!
+  GOTO ERROR
+)
+SET rootdir=%~dp0
+SET cwdir=%CD%
+SET target=windows_x86_64
+SET prefix=%rootdir%%target%
+SET /P version=<"%rootdir%version.txt"
+RD /S /Q "%rootdir%%version%"
+MD "%rootdir%%version%"
+XCOPY "%rootdir%..\%version%" "%rootdir%%version%" /S /Y
+RD /S /Q "%rootdir%project"
+MD "%rootdir%project"
+CD /D "%rootdir%project"
+cmake -C "%rootdir%CMakeLists.txt" -DCMAKE_INSTALL_PREFIX="%prefix%" -G %GENERATOR% "%rootdir%%version%"
+cmake "%rootdir%%version%"
+cmake --build . --target install --config Release --clean-first
+MD "%rootdir%..\target\include\%target%"
+XCOPY "%prefix%\include" "%rootdir%..\target\include\%target%" /S /Y
+MD "%rootdir%..\target\lib\%target%%target_lib_suffix%"
+COPY /Y "%prefix%\lib\zlibstatic.lib" "%rootdir%..\target\lib\%target%%target_lib_suffix%"
+CD /D "%cwdir%"
+RD /S /Q "%rootdir%%version%"
+RD /S /Q "%rootdir%project"
+RD /S /Q "%prefix%"
+GOTO :EOF
+
+:ERROR
+PAUSE
